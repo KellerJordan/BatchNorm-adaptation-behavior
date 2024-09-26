40a1088ec4f6d9ad7e847aa2662e1ebf0b6393ce
KellerJordan <kellerjordan1998@gmail.com>
2024-09-26 08:05:09 +0000

    .

--- make_figure.py
+++ make_figure.py
@@ -15,12 +15,13 @@ labels = [
 ]
 
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+plt.figure(figsize=(6.5, 5.5))
 for i, k in labels:
     plt.plot(results[i], label=k,
              marker=['^', 'd', 'o', 'P'][i], markersize=6,
              linestyle=['solid', 'dashed'][int('reset' in k)])
 plt.xticks(list(range(len(corruptions1))), corruptions1, rotation='vertical', fontsize=12)
-plt.legend(fontsize=11)
+plt.legend(fontsize=11, loc='lower right')
 plt.ylabel('Accuracy', fontsize=14)
 plt.xlabel('Evaluation distribution', fontsize=14)
 plt.title('BatchNorm and Norm-Free networks do not have\nsignificantly different domain adaptation behavior',
